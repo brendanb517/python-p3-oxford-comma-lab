f3c91ec003d6b874084ebb6dd325ceeeb5b051f4
Brendan <brendan.b517@gmail.com>
2023-04-26 23:49:53 -0400

    Passed all tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) >= 3:
+        last_item = items.pop()
+        new_string = ", ".join(items) + f", and {last_item}"
+        return new_string
